8f8fea904ce8b3d146328488ad4d88f2f011540e
MathiasWeberComCave <q-mwb@gmx.de>
2022-11-11 13:02:27 +0100

    funktionsschachtelung entsprechend srp

--- script_01.js
+++ script_01.js
@@ -2,3 +2,16 @@
 
 /***** Funktionen 01 *****/
 
+//  callee
+function test() {
+    console.log ("Hallo");
+}
+
+//  call
+// test();
+
+function ausgabeName (personName, personNachname) //Parameter
+{
+    console.log ("Hallo " + personName + " " + personNachname);
+}
+ ausgabeName("Sabine", "Eitel") //Aufruf mit Argunment
